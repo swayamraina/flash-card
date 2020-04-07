9cb17b0347e9ad840f3c09a44aa94bf6c8753650
swayamraina <swayamraina@gmail.com>
2020-04-07 23:05:06 +0530

    ds to hold the inncoming links and tags

---
+++ server/src/main/java/dev/swayamraina/flashcard/core/Queue.java
@@ -0,0 +1,47 @@
+package dev.swayamraina.flashcard.core;
+
+import java.util.Objects;
+
+public class Queue <E> {
+
+    private static final int QUEUE_SIZE = 16;
+
+    private CircularQueue<E> current;
+    public CircularQueue<E> current () { return this.current; }
+
+    private CircularQueue<E> reserved;
+    public CircularQueue<E> reserved () { return this.reserved; }
+
+    private CircularQueue<E> retry;
+    public CircularQueue<E> retry () { return this.retry; }
+
+    private CircularQueue<E> failed;
+    public CircularQueue<E> failed () { return this.failed; }
+
+
+    public Queue () {
+        this.current = new CircularQueue(QUEUE_SIZE);
+        this.reserved = new CircularQueue(QUEUE_SIZE);
+        this.retry = new CircularQueue(QUEUE_SIZE);
+        this.failed = new CircularQueue(QUEUE_SIZE);
+    }
+
+
+    synchronized public QCode add (E element) {
+        if (current.full() && reserved.full()) return QCode.RESERVE_FULL;
+        CircularQueue<E> queue = current.notFull() ? current : reserved;
+        queue.add(element);
+        return QCode.SUCCESS;
+    }
+
+    synchronized public E poll () {
+        E ec = null, er = null;
+        if (current.notEmpty()) {
+            ec = current.poll();
+            er = reserved.poll();
+            if (Objects.nonNull(er)) current.add(er);
+        }
+        return ec;
+    }
+
+}
