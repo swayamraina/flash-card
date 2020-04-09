aee1dafc86736582c9d6d58843bac02e6799e093
swayamraina <swayamraina@gmail.com>
2020-04-09 19:23:52 +0530

    response codes

---
+++ server/src/main/java/dev/swayamraina/flashcard/service/github/GCode.java
@@ -0,0 +1,15 @@
+package dev.swayamraina.flashcard.service.github;
+
+public enum GCode {
+
+    INVALID,
+
+    EXISTS,
+
+    CREATED,
+
+    UPDATED,
+
+    FAILED,
+
+}
---
+++ server/src/main/java/dev/swayamraina/flashcard/storage/SCode.java
@@ -0,0 +1,15 @@
+package dev.swayamraina.flashcard.storage;
+
+public enum SCode {
+
+    UNKNOWN,
+
+    ERROR,
+
+    SAVED_TO_FILE,
+
+    SAVED_TO_CACHE,
+
+    SAVED
+
+}
