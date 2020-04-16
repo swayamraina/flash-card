30399cb81bcd6024ee9b70face5e3eaf270798c3
swayamraina <swayamraina@gmail.com>
2020-04-16 23:35:19 +0530

    github data resource

---
+++ server/src/main/java/dev/swayamraina/flashcard/service/github/entity/Resource.java
@@ -0,0 +1,24 @@
+package dev.swayamraina.flashcard.service.github.entity;
+
+import dev.swayamraina.flashcard.service.github.GCode;
+
+public class Resource {
+
+    private GCode valid;
+    public boolean invalid () { return GCode.INVALID == this.valid; }
+    public boolean valid () { return GCode.INVALID != this.valid; }
+    public boolean exists () { return GCode.EXISTS == this.valid; }
+
+    private String sha;
+    public String sha () { return sha; }
+
+    private String content;
+    public String content () { return content; }
+
+    public Resource (GCode valid, String sha, String content) {
+        this.valid = valid;
+        this.sha = sha;
+        this.content = content;
+    }
+
+}
