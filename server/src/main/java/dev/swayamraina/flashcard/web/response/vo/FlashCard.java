d72f8587603bfc1dd5256ea1ec6dd9a04b884b0e
swayamraina <swayamraina@gmail.com>
2020-04-17 23:52:33 +0530

    define getters for jackson to pick vars

--- server/src/main/java/dev/swayamraina/flashcard/web/response/vo/FlashCard.java
+++ server/src/main/java/dev/swayamraina/flashcard/web/response/vo/FlashCard.java
@@ -8,9 +8,11 @@ public class FlashCard {
 
     private String url;
     public String url () { return url; }
+    public String getUrl () { return url; }
 
     private List<String> tags;
     public List<String> tags () { return tags; }
+    public List<String> getTags () { return tags; }
 
 
 
