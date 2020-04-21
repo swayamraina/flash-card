a4c8fe195d6a5a95b1eebbf1de63b5eb70fb5f45
swayamraina <swayamraina@gmail.com>
2020-04-22 00:09:20 +0530

    fix: hash ring storage

--- server/src/main/java/dev/swayamraina/flashcard/storage/worker/HashRing.java
+++ server/src/main/java/dev/swayamraina/flashcard/storage/worker/HashRing.java
@@ -18,31 +18,36 @@ import static dev.swayamraina.flashcard.utils.Constants.NEWLINE;
 
 @Service public class HashRing {
 
+
     private static final int seed = 123456789;
     private static final Charset utf8 = StandardCharsets.UTF_8;
 
+
     private HashFunction function;
     private Github github;
+
 
     @Autowired public HashRing (Github github)  {
         function = Hashing.murmur3_32(seed);
         this.github = github;
     }
 
+
     public SCode add (String url) {
         boolean valid, exists;
         int hash = hash(url);
         String api = github.hashRingUrl(hash);
         Optional<Resource> resource = github.read(api);
-        valid = resource.isPresent() && resource.get().valid();
-        if (valid) {
-            exists = resource.get().exists();
-            Request request = github.request(url, resource.get());
-            if (request.invalid()) return SCode.ERROR;
-            resource = exists ? github.update(api, request) : github.create(api, request);
-        }
+        exists = resource.isPresent() && resource.get().exists();
+        Request request = github.request(url, resource.get());
+
+        if (request.invalid())
+            return SCode.ERROR;
+
+        resource = exists ? github.update(api, request) : github.create(api, request);
         return resource.isPresent() && resource.get().valid() ? SCode.SAVED_TO_HASH_RING : SCode.ERROR;
     }
+
 
     public boolean exists (String url) {
         boolean exists = false;
@@ -61,8 +66,10 @@ import static dev.swayamraina.flashcard.utils.Constants.NEWLINE;
         return exists;
     }
 
+
     private int hash (String url) {
         return Math.abs(function.hashString(url, utf8).asInt());
     }
+
 
 }
