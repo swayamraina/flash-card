a5e2edfeae5aa218e0997975b0064fd497af489d
swayamraina <swayamraina@gmail.com>
2020-05-10 18:04:27 +0530

    added auth handler

---
+++ server/src/main/java/dev/swayamraina/flashcard/web/routes/interceptor/Authentication.java
@@ -0,0 +1,31 @@
+package dev.swayamraina.flashcard.web.routes.interceptor;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@Component public class Authentication extends HandlerInterceptorAdapter {
+
+    private static final String USER = "x-user-id";
+    private static final String PWD = "x-auth-id";
+
+    private Config config;
+
+    @Autowired public Authentication (@Qualifier ("auth") Config config) { this.config = config; }
+
+
+    @Override public boolean preHandle (
+            HttpServletRequest request,
+            HttpServletResponse response,
+            Object handler) {
+
+        String user = request.getHeader(USER);
+        String pwd = request.getHeader(PWD);
+        return config.user().equals(user) && config.auth().equals(pwd);
+    }
+
+}
